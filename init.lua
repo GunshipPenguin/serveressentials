6b6ec40b45f02a1627279c4820e9c63d7e7e7b71
GunshipPenguin <167rhys@gmail.com>
2015-12-22 23:23:16 -0800

    Make code follow Minetest lua code style guidlines

--- init.lua
+++ init.lua
@@ -1,50 +1,62 @@
 --[[
 Server Essentials mod for Minetest by GunshipPenguin
+
 To the extent possible under law, the author(s)
 have dedicated all copyright and related and neighboring rights
 to this software to the public domain worldwide. This software is
 distributed without any warranty.
-]]
+--]]
 
 players = {}
-checkTimer = 0
+check_timer = 0
 dofile(minetest.get_modpath("serveressentials") .. "/settings.lua")
 
-minetest.register_privilege("godmode", "Player can use godmode with the /godmode command")
-minetest.register_privilege("broadcast", "Player can use /broadcast command")
-minetest.register_privilege("kill", "Player can kill other players with the /kill command")
-minetest.register_privilege("heal", "Player can heal other players with the /heal command")
-minetest.register_privilege("top", "Player can use the /top command")
-minetest.register_privilege("setspeed", "Player can set player speeds with the /setspeed command")
-minetest.register_privilege("whois", "Player can view other player's network information with the /whois command")
-minetest.register_privilege("chatspam", "Player can send chat messages longer than MAX_CHAT_MSG_LENGTH without being kicked")
+minetest.register_privilege("godmode",
+		"Player can use godmode with the /godmode command")
+minetest.register_privilege("broadcast",
+		"Player can use /broadcast command")
+minetest.register_privilege("kill",
+		"Player can kill other players with the /kill command")
+minetest.register_privilege("heal",
+		"Player can heal other players with the /heal command")
+minetest.register_privilege("top",
+		"Player can use the /top command")
+minetest.register_privilege("setspeed",
+		"Player can set player speeds with the /setspeed command")
+minetest.register_privilege("whois",
+		"Player can view other player's network information with the /whois command")
+minetest.register_privilege("chatspam",
+		"Player can send chat messages longer than MAX_CHAT_MSG_LENGTH without being kicked")
 
 if AFK_CHECK then
-	minetest.register_privilege("canafk", "Player can remain afk without being kicked")
+	minetest.register_privilege("canafk",
+		"Player can remain afk without being kicked")
 end
 
 if minetest.setting_get("static_spawnpoint") then
-	minetest.register_privilege("spawn", "Player can teleport to static spawnpoint using /spawn command")
-	
+	minetest.register_privilege("spawn",
+			"Player can teleport to static spawnpoint using /spawn command")
+
 	minetest.register_chatcommand("spawn", {
 		params = "",
 		description = "Teleport to static spawnpoint",
-		privs = {spawn = true}, 
-		func = function(playerName, param)
-			local spawn_func = function(playerName)
-				local spawnPoint = minetest.setting_get("static_spawnpoint") 
-				minetest.get_player_by_name(playerName):setpos(minetest.string_to_pos(spawnPoint))
+		privs = {spawn = true},
+		func = function(player_name, param)
+			local spawn_func = function(player_name)
+				local spawn_point = minetest.setting_get("static_spawnpoint")
+				local player = minetest.get_player_by_name(player_name)
+				player:setpos(minetest.string_to_pos(spawn_point))
 				return
 			end
-			
-			
+
 			if SPAWN_DELAY > 0 then
-				minetest.chat_send_player(playerName, "Teleporting you to spawn in " .. SPAWN_DELAY .. " seconds")
-				minetest.after(SPAWN_DELAY, spawn_func, playerName)
+				minetest.chat_send_player(player_name,
+						"Teleporting you to spawn in " .. SPAWN_DELAY .. " seconds")
+				minetest.after(SPAWN_DELAY, spawn_func, player_name)
 			else
-				minetest.chat_send_player(playerName, "Teleporting you to spawn")
-				spawn_func(playerName)
-			end 
+				minetest.chat_send_player(player_name, "Teleporting you to spawn")
+				spawn_func(player_name)
+			end
 		end
 	})
 end
@@ -53,23 +65,23 @@ minetest.register_chatcommand("ping", {
 	params = "",
 	description = "Pong!",
 	privs = {},
-	func = function(playerName, text)
-		minetest.chat_send_player(playerName, "Pong!")
+	func = function(player_name, text)
+		minetest.chat_send_player(player_name, "Pong!")
 	end
 })
 
 minetest.register_chatcommand("motd", {
-	params = "", 
+	params = "",
 	description = "Display server motd",
-	privs = {}, 
-	func = function(playerName, text)
+	privs = {},
+	func = function(player_name, text)
 		local motd = minetest.setting_get("motd")
 		if motd == nil or motd == "" then
-			minetest.chat_send_player(playerName, "Motd has not been set")
+			minetest.chat_send_player(player_name, "Motd has not been set")
 		else
-			minetest.chat_send_player(playerName, motd)
+			minetest.chat_send_player(player_name, motd)
 		end
-	end 
+	end
 })
 
 
@@ -77,8 +89,8 @@ minetest.register_chatcommand("clearinv", {
 	params = "";
 	description = "Clear your inventory",
 	privs = {},
-	func = function(playerName, text)
-		local inventory = minetest.get_player_by_name(playerName):get_inventory()
+	func = function(player_name, text)
+		local inventory = minetest.get_player_by_name(player_name):get_inventory()
 		inventory:set_list("main", {})
 	end,
 })
@@ -87,25 +99,25 @@ minetest.register_chatcommand("broadcast", {
 	params = "<text>",
 	description = "Broadcast message to server",
 	privs = {broadcast = true},
-	func = function(playerName, text)
+	func = function(player_name, text)
 		minetest.chat_send_all(BROADCAST_PREFIX .. " " ..  text)
 		return
 	end,
 })
 
 minetest.register_chatcommand("kill", {
-	params = "<playerName>",
+	params = "<player_name>",
 	description = "kill specified player",
 	privs = {kill = true},
-	func = function(playerName, param)
-		
+	func = function(player_name, param)
+
 		if #param==0 then
-			minetest.chat_send_player(playerName, "You must supply a player name")
+			minetest.chat_send_player(player_name, "You must supply a player name")
 		elseif players[param] then
-			minetest.chat_send_player(playerName, "Killing player " .. param)
+			minetest.chat_send_player(player_name, "Killing player " .. param)
 			minetest.get_player_by_name(param):set_hp(0)
 		else
-			minetest.chat_send_player(playerName, "Player " .. param .. " cannot be found")
+			minetest.chat_send_player(player_name, "Player " .. param .. " cannot be found")
 		end
 		return
 	end
@@ -115,22 +127,22 @@ minetest.register_chatcommand("top", {
 	params = "",
 	description = "Teleport to topmost block at your current position",
 	privs = {top = true},
-	func = function(playerName, param)
-		currPos = minetest.get_player_by_name(playerName):getpos()
-		currPos["y"] = math.ceil(currPos["y"]) + 0.5
-		
-		while minetest.get_node(currPos)["name"] ~= "ignore" do
-			currPos["y"] = currPos["y"] + 1
-		end
-		
-		currPos["y"] = currPos["y"] - 0.5
+	func = function(player_name, param)
+		curr_pos = minetest.get_player_by_name(player_name):getpos()
+		curr_pos["y"] = math.ceil(curr_pos["y"]) + 0.5
 
-		while minetest.get_node(currPos)["name"] == "air" do
-			currPos["y"] = currPos["y"] - 1
+		while minetest.get_node(curr_pos)["name"] ~= "ignore" do
+			curr_pos["y"] = curr_pos["y"] + 1
 		end
-		currPos["y"] = currPos["y"] + 0.5
-		
-		minetest.get_player_by_name(playerName):setpos(currPos)
+
+		curr_pos["y"] = curr_pos["y"] - 0.5
+
+		while minetest.get_node(curr_pos)["name"] == "air" do
+			curr_pos["y"] = curr_pos["y"] - 1
+		end
+		curr_pos["y"] = curr_pos["y"] + 0.5
+
+		minetest.get_player_by_name(player_name):setpos(curr_pos)
 		return
 	end
 })
@@ -138,38 +150,39 @@ minetest.register_chatcommand("top", {
 minetest.register_chatcommand("killme", {
 	params = "",
 	description = "Kill yourself",
-	func = function(playerName, param)
-		minetest.chat_send_player(playerName, "Killing Player " .. playerName)
-		minetest.get_player_by_name(playerName):set_hp(0)
+	func = function(player_name, param)
+		minetest.chat_send_player(player_name, "Killing Player " .. player_name)
+		minetest.get_player_by_name(player_name):set_hp(0)
 		return
 	end
 })
 
 minetest.register_chatcommand("heal", {
-	params = "[playerName]",
+	params = "[player_name]",
 	description = "Heal specified player, heals self if run without arguments",
 	privs = {heal = true},
-	func = function(playerName, param)
-		
-		if #param==0 then
-			minetest.chat_send_player(playerName, "Healing player " .. playerName)
-			minetest.get_player_by_name(playerName):set_hp(20)
-		elseif players[playerName] and param and players[param] then
-			minetest.chat_send_player(playerName, "Healing player " .. param)
+	func = function(player_name, param)
+
+		if #param == 0 then
+			minetest.chat_send_player(player_name, "Healing player " .. player_name)
+			minetest.get_player_by_name(player_name):set_hp(20)
+		elseif players[player_name] and param and players[param] then
+			minetest.chat_send_player(player_name, "Healing player " .. param)
 			minetest.get_player_by_name(param):set_hp(20)
 		else
-			minetest.chat_send_player(playerName, "Player " .. param .. " cannot not be found")
+			minetest.chat_send_player(player_name, "Player " .. param .. " cannot not be found")
 		end
 		return
 	end
 })
 
 minetest.register_chatcommand("gettime", {
-	params = "", 
-	description = "Get the current time of day", 
+	params = "",
+	description = "Get the current time of day",
 	privs = {},
-	func = function(playerName, param)
-		minetest.chat_send_player(playerName, "Current time of day is: " .. tostring(math.ceil(minetest.get_timeofday()*24000)))
+	func = function(player_name, param)
+		minetest.chat_send_player(player_name, "Current time of day is: " ..
+				tostring(math.ceil(minetest.get_timeofday() * 24000)))
 		return
 	end
 })
@@ -178,35 +191,55 @@ minetest.register_chatcommand("godmode", {
 	params = "",
 	description = "Toggle godmode",
 	privs = {godmode = true},
-	func = function(playerName, param)
-		players[playerName]["godMode"] = not players[playerName]["godMode"];
-		if players[playerName]["godMode"] then
-			minetest.chat_send_player(playerName, "Godmode is now on")
+	func = function(player_name, param)
+		players[player_name]["god_mode"] = not players[player_name]["god_mode"];
+		if players[player_name]["god_mode"] then
+			minetest.chat_send_player(player_name, "Godmode is now on")
 		else
-			minetest.chat_send_player(playerName, "Godmode is now off")
+			minetest.chat_send_player(player_name, "Godmode is now off")
 		end
 		return
 	end
 })
 
 minetest.register_chatcommand("setspeed", {
-	params = "<speed> [playerName]",
+	params = "<speed> [player_name]",
 	description = "Set your or somebody else's walking speed",
 	privs = {setspeed = true},
-	func = function(playerName, param)
+	func = function(player_name, param)
 		param = string.split(param, " ")
-		
-		if #param==0 or tonumber(param[1]) == nil then
-			minetest.chat_send_player(playerName, "You must supply proper a speed")
-		elseif players[playerName] and players[param[2]] then
-			minetest.chat_send_player(playerName, "Setting player " .. param[2] .. "'s walking speed to " .. tostring(param[1]) .. " times normal speed")
-			minetest.chat_send_player(param[2], "Your walking speed has been set to " .. param[1] .. " times normal speed")
-			minetest.get_player_by_name(param[2]):set_physics_override({speed=tonumber(param[2]), jump=1.0, gravity=1.0})
-		elseif players[playerName] and not param[2] then
-			minetest.chat_send_player(playerName, "Setting player " .. playerName .. "'s walking speed to " .. tostring(param[1]) .. " times normal speed.")
-			minetest.get_player_by_name(playerName):set_physics_override({speed=tonumber(param[1]), jump=1.0, gravity=1.0})
+
+		if #param == 0 or tonumber(param[1]) == nil then
+			minetest.chat_send_player(player_name, "You must supply proper a speed")
+		elseif players[player_name] and players[param[2]] then
+			minetest.chat_send_player(player_name, "Setting player " ..
+					param[2] ..
+					"'s walking speed to " ..
+					tostring(param[1]) ..
+					" times normal speed")
+			minetest.chat_send_player(param[2], "Your walking speed has been set to "
+					.. param[1] ..
+					" times normal speed")
+			minetest.get_player_by_name(param[2]):set_physics_override({
+					speed=tonumber(param[2]),
+					jump=1.0,
+					gravity=1.0
+			})
+		elseif players[player_name] and not param[2] then
+			minetest.chat_send_player(player_name, "Setting player " ..
+					player_name ..
+					"'s walking speed to " ..
+					tostring(param[1]) ..
+					" times normal speed.")
+			minetest.get_player_by_name(player_name):set_physics_override({
+					speed=tonumber(param[1]),
+					jump=1.0,
+					gravity=1.0
+			})
 		else
-			minetest.chat_send_player(playerName, "Player ".. param[2] .. " cannot be found")
+			minetest.chat_send_player(player_name, "Player " ..
+					param[2] ..
+					" cannot be found")
 		end
 		return
 	end
@@ -215,32 +248,36 @@ minetest.register_chatcommand("setspeed", {
 minetest.register_chatcommand("whatisthis", {
 	params = "",
 	description = "Get itemstring of wielded item",
-	func = function(playerName, param)
-		minetest.chat_send_player(playerName, minetest.get_player_by_name(playerName):get_wielded_item():to_string())
+	func = function(player_name, param)
+		local player = minetest.get_player_by_name(player_name)
+		minetest.chat_send_player(player_name, player:get_wielded_item():to_string())
 		return
 	end
 })
 
 minetest.register_chatcommand("whois", {
-	params = "<playerName>",
+	params = "<player_name>",
 	description = "Get network information of player",
 	privs = {whois = true},
-	func = function(playerName, param)
+	func = function(player_name, param)
 		if not param or not players[param] then
-			minetest.chat_send_player(playerName, "Player " .. param .. " was not found")
+			minetest.chat_send_player(player_name, "Player " .. param .. " was not found")
 			return
 		end
 		playerInfo = minetest.get_player_information(param)
-		minetest.chat_send_player(playerName, param .. " - IP address - " .. playerInfo["address"])
-		minetest.chat_send_player(playerName, param .. " - Avg rtt - " .. playerInfo["avg_rtt"])
-		minetest.chat_send_player(playerName, param .. " - Connection uptime (seconds) - " .. playerInfo["connection_uptime"])
+		minetest.chat_send_player(player_name, param ..
+				" - IP address - " .. playerInfo["address"])
+		minetest.chat_send_player(player_name, param ..
+				" - Avg rtt - " .. playerInfo["avg_rtt"])
+		minetest.chat_send_player(player_name, param ..
+				" - Connection uptime (seconds) - " .. playerInfo["connection_uptime"])
 		return
 	end
 })
 
 minetest.register_on_joinplayer(function(player)
 	players[player:get_player_name()] = {
-		lastAction = minetest.get_gametime(),
+		last_action = minetest.get_gametime(),
 		godmode = false
 	}
 end)
@@ -259,48 +296,56 @@ minetest.register_on_newplayer(function(player)
 end)
 
 minetest.register_on_chat_message(function(name, message)
-	if KICK_CHATSPAM and not minetest.check_player_privs(name, {chatspam=true}) and string.len(message) > MAX_CHAT_MSG_LENGTH then
-		minetest.kick_player(name, "You were kicked because you sent a chat message longer than " .. MAX_CHAT_MSG_LENGTH .. " characters. This is to prevent chat spamming.")
+	if KICK_CHATSPAM and not minetest.check_player_privs(name, {chatspam=true}) and
+			string.len(message) > MAX_CHAT_MSG_LENGTH then
+		minetest.kick_player(name,
+				"You were kicked because you sent a chat message longer than " ..
+				MAX_CHAT_MSG_LENGTH ..
+				" characters. This is to prevent chat spamming.")
 		return true
 	end
-	return 
+	return
 end)
 
 minetest.register_globalstep(function(dtime)
-	
-	--Loop through all connected players
+	-- Loop through all connected players
 	for _,player in ipairs(minetest.get_connected_players()) do
-		local playerName = player:get_player_name()
-		
-		if players[playerName] then --Only continue if the player has an entry in the players table
-		
-			--Check for afk players
-			if AFK_CHECK and not minetest.check_player_privs(playerName, {canafk=true}) then
-				checkTimer = checkTimer + dtime
-				if checkTimer > AFK_CHECK_INTERVAL then
-					checkTimer = 0
-					
-					--Kick player if he/she has been inactive for longer than MAX_INACTIVE_TIME seconds
-					if players[playerName]["lastAction"] + MAX_AFK_TIME < minetest.get_gametime() then 
-						minetest.kick_player(playerName, "Kicked for inactivity")
+		local player_name = player:get_player_name()
+
+		-- Only continue if the player has an entry in the players table
+		if players[player_name] then
+
+			-- Check for afk players
+			if AFK_CHECK and not minetest.check_player_privs(player_name, {canafk=true}) then
+				check_timer = check_timer + dtime
+				if check_timer > AFK_CHECK_INTERVAL then
+					check_timer = 0
+
+					-- Kick player if he/she has been inactive for longer than MAX_INACTIVE_TIME seconds
+					if players[player_name]["last_action"] + MAX_AFK_TIME <
+							minetest.get_gametime() then
+						minetest.kick_player(player_name, "Kicked for inactivity")
 					end
-					
-					--Warn player if he/she has less than WARN_TIME seconds to move or be kicked
-					if players[playerName]["lastAction"] + MAX_AFK_TIME - AFK_WARN_TIME < minetest.get_gametime() then
-						minetest.chat_send_player(playerName, "Warning, you have " .. tostring(players[playerName]["lastAction"] + MAX_AFK_TIME - minetest.get_gametime()) .. " seconds to move or be kicked")
+
+					-- Warn player if he/she has less than WARN_TIME seconds to move or be kicked
+					if players[player_name]["last_action"] + MAX_AFK_TIME - AFK_WARN_TIME <
+							minetest.get_gametime() then
+						minetest.chat_send_player(player_name, "Warning, you have " ..
+								tostring(players[player_name]["last_action"] + MAX_AFK_TIME - minetest.get_gametime())
+								.. " seconds to move or be kicked")
 					end
 				end
-				
-				--Check if this player is doing an action
+
+				-- Check if this player is doing an action
 				for _,keyPressed in pairs(player:get_player_control()) do
 					if keyPressed then
-						players[playerName]["lastAction"] = minetest.get_gametime()
+						players[player_name]["last_action"] = minetest.get_gametime()
 					end
 				end
 			end
-			
-			--Check if player has godmode turned on
-			if players[playerName]["godMode"] then
+
+			-- Check if player has godmode turned on
+			if players[player_name]["god_mode"] then
 				player:set_hp(20)
 				player:set_breath(11)
 			end
--- settings.lua
+++ settings.lua
@@ -1,14 +1,23 @@
-AFK_CHECK = true --Whether or not to automatically kick afk players
-MAX_AFK_TIME = 300 --Max time allowed afk before kick 
-AFK_CHECK_INTERVAL = 1 --Number of seconds between activity checks
-AFK_WARN_TIME = 20 --Number of seconds before being kicked that a player will start to be warned
+-- Whether or not to automatically kick afk players
+AFK_CHECK = true
+-- Max time allowed afk before kick
+MAX_AFK_TIME = 300
+-- Number of seconds between activity checks
+AFK_CHECK_INTERVAL = 1
+-- Number of seconds before being kicked that a player will start to be warned
+AFK_WARN_TIME = 20
 
-SHOW_FIRST_TIME_JOIN_MSG = true --Whether or not to show FIRST_TIME_JOIN_MSG if a new player joins
-FIRST_TIME_JOIN_MSG = " has joined the server for the first time, Welcome!" --Message to broadcast to all players when a new player joins the server, will follow the players name
+-- Whether or not to show FIRST_TIME_JOIN_MSG if a new player joins
+SHOW_FIRST_TIME_JOIN_MSG = true
+-- Message to broadcast to all players when a new player joins the server, will follow the players name
+FIRST_TIME_JOIN_MSG = " has joined the server for the first time, Welcome!"
 
-BROADCAST_PREFIX = "[SERVER]" --All messages sent with the /broadcast command will be prefixed with this
+-- All messages sent with the /broadcast command will be prefixed with this
+BROADCAST_PREFIX = "[SERVER]"
 
-KICK_CHATSPAM = true --If true, players who send a chat message longer than MAX_CHAT_MSG_LENGTH will be kicked
+-- If true, players who send a chat message longer than MAX_CHAT_MSG_LENGTH will be kicked
+KICK_CHATSPAM = true
 MAX_CHAT_MSG_LENGTH = 400
 
-SPAWN_DELAY = 0 --Delay in seconds that will occur between issuing /spawn command and actually being teleported to the static_spawnpoint
+-- Delay in seconds that will occur between issuing /spawn command and actually being teleported to the static_spawnpoint
+SPAWN_DELAY = 0
